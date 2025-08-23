cfcee6bc8cae7d56caffd0a69283d51c667baf78
Luis Enrique Castaneda Donaire <luis.castaneda@intellectsystem.net>
2025-08-23 15:03:05 -0600

    Primera parte de docuemntos

---
+++ Inmob-Salesforce/force-app/main/default/lwc/imprimirDocumentos/imprimirDocumentos.js
@@ -0,0 +1,28 @@
+import { LightningElement,api, track, wire} from 'lwc';
+import CONTRACT_OBJECT from '@salesforce/schema/Quote';
+import { getRecord } from 'lightning/uiRecordApi';
+import NAME_FIELD from '@salesforce/schema/Contrato__c.Name';
+
+export default class ImprimirDocumentos extends LightningElement {
+    @api recordId;
+    @track allData = {};
+    @track documents= [];
+
+    @wire(getRecord, { recordId: '$recordId', fields: [NAME_FIELD] })
+    wiredAccount({data, error}) {
+        if (data) {
+            this.allData.title = data.fields.Name.value;
+            this.documents.push({name : "Autorización de Buró", url: "/apex/AutorizacionBuroPdf?id=" + this.recordId})
+            this.documents.push({name : "Carta de Responsabilidad Cliente", url: "/apex/ResponsabilidadClientePdf?id=" + this.recordId})
+            this.documents.push({name : "Documento 3", url: "/apex/AutorizacionBuroPdf?id=" + this.recordId})
+            this.documents.push({name : "Documento 4", url: "/apex/nada?id=" + this.recordId})
+            this.documents.push({name : "Documento 5", url: "/apex/nada?id=" + this.recordId})
+            this.documents.push({name : "Documento 6", url: "/apex/nada?id=" + this.recordId})
+            this.documents.push({name : "Documento 7", url: "/apex/nada?id=" + this.recordId})
+            this.documents.push({name : "Documento 8", url: "/apex/nada?id=" + this.recordId})
+            this.documents.push({name : "Documento 9", url: "/apex/nada?id=" + this.recordId})
+            this.documents.push({name : "Documento 10", url: "/apex/nada?id=" + this.recordId})
+        }
+    }
+
+}
