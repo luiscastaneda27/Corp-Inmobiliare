774fd58f68b35d1effed0d102c47d5e597374143
Luis Enrique Castaneda Donaire <luis.castaneda@intellectsystem.net>
2025-09-22 19:33:11 -0600

    Agrego corrección en pdf

--- Inmob-Salesforce/force-app/main/default/lwc/imprimirDocumentos/imprimirDocumentos.js
+++ Inmob-Salesforce/force-app/main/default/lwc/imprimirDocumentos/imprimirDocumentos.js
@@ -14,14 +14,14 @@ export default class ImprimirDocumentos extends LightningElement {
             this.allData.title = data.fields.Name.value;
             this.documents.push({name : "Autorización de Buró", url: "/apex/AutorizacionBuroPdf?id=" + this.recordId})
             this.documents.push({name : "Carta de Responsabilidad Cliente", url: "/apex/ResponsabilidadClientePdf?id=" + this.recordId})
-            this.documents.push({name : "Declaración Y Autorización Pep Cii", url: "/apex/DeclaracionAutorizacionPepPdf?id=" + this.recordId})
+            this.documents.push({name : "Declaración y Autorización PEP", url: "/apex/DeclaracionAutorizacionPepPdf?id=" + this.recordId})
             this.documents.push({name : "Entrevistas De Créditos", url: "/apex/EntrevistaCreditoPdf?id=" + this.recordId})
-            this.documents.push({name : "Especificaciones Técnias", url: "/apex/EspecificacionesTecnicasPdf?id=" + this.recordId})
-            this.documents.push({name : "Documento 6", url: "/apex/nada?id=" + this.recordId})
+            this.documents.push({name : "Especificaciones Técnicas", url: "/apex/EspecificacionesTecnicasPdf?id=" + this.recordId})
+            /*this.documents.push({name : "Documento 6", url: "/apex/nada?id=" + this.recordId})
             this.documents.push({name : "Documento 7", url: "/apex/nada?id=" + this.recordId})
             this.documents.push({name : "Documento 8", url: "/apex/nada?id=" + this.recordId})
             this.documents.push({name : "Documento 9", url: "/apex/nada?id=" + this.recordId})
-            this.documents.push({name : "Documento 10", url: "/apex/nada?id=" + this.recordId})
+            this.documents.push({name : "Documento 10", url: "/apex/nada?id=" + this.recordId})*/
         }
     }
 
